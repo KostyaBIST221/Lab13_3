c26e50823a3657f28169463968ae5e232c6d6916
KostyaBIST221 <ksapov24@mail.ru>
2022-11-30 21:03:50 +0300

    Initial Commit

---
+++ Lab13_3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab13_3
+//
+//  Created by kostya on 30.11.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
